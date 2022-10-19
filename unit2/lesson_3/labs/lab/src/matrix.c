d49b7264cd7465c1f857db013d22d0b505709ff1
abozahra <abozahra@gmail.com>
2022-10-20 00:37:01 +0200

    second commit add homework

---
+++ unit2/lesson_3/labs/lab/src/matrix.c
@@ -0,0 +1,25 @@
+/*
+ ============================================================================
+ Name        : matrix.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+
+int main()
+{
+	for(int i=0;i<=9;i++)
+	{
+		for(int x=i;x<=9;x++)
+		{
+			printf("%d",x);
+		}
+		printf("\n");
+	}
+
+}
